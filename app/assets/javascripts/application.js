0ead9389bf0dea20abc12a0acc5f6963db87f8ea
AR8Stefan <stefan.piatkov@gmail.com>
2017-07-21 15:22:48 +1000

    add styles to navbar and main challenge page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// require jquery
-// require jquery_ujs
-//
 //= require jquery3
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require challenges
 //= require_tree .
 
 
