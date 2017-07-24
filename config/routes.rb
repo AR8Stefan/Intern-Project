d8020c0558384ed30642eb2e669b5a35d51d44ed
AR8Stefan <stefan.piatkov@gmail.com>
2017-07-24 17:11:30 +1000

    add styles to edit and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :challenges
+  resources :challenges do
+  	collection do
+  		get :index
+  	end
+  end
+
+  root 'challenges#index'
   devise_for :teachers, skip: [:registrations]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
