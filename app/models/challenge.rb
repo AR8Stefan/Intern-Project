e12997bc66e171986d9849dc031c58fa6ecb00c8
AR8Stefan <stefan.piatkov@gmail.com>
2017-08-01 11:08:20 +1000

    tags being sent when new challenge is made

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,15 +1,15 @@
 class Challenge < ApplicationRecord
 	ratyrate_rateable "rate"
-	has_many :tags
-	accepts_nested_attributes_for :tags
+	# has_many :tags
+	# accepts_nested_attributes_for :tags
 	acts_as_taggable
 	# acts_as_taggable_on :tags 
 
-	# def tags=(names)
-	#   self.tags = names.split(",").map do |name|
-	#       Tag.where(name: name.strip).first_or_create!
-	#   end
-	# end
+	def tags=(names)
+	  self.tags = names.split(",").map do |name|
+	      Tag.where(name: name.strip).first_or_create!
+	  end
+	end
 
 	# def tags
 	#   self.tags.map(&:name).join(", ")
